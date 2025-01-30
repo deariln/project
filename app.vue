373d57c96c13389af9ed759dc9ced5ffd6ba64cb
deariln <deariln@mail.ru>
2025-01-30 19:53:15 +0300

    1

--- app.vue
+++ app.vue
@@ -1,12 +1,38 @@
 <template>
-  <h1>
-    gfwrgraeh
-  </h1>
+  <section class="body">
+    <div class="div-butt_main">
+      <button class="button-main">
+        ГЛАВНАЯ СТРАНИЦА 
+      </button>
+    </div>
+    <div class="div-matrix_butt">
+       <button class="matrix-fate">
+        Матрица судьбы
+       </button>
+       <button class="matrix-year">
+        Матрица года
+       </button>
+    </div>
+
+ 
+  <div class="container">
+
+    <form action="" class="form">
+      
+      <input type="text" placeholder="Name" class="form__input" id="name" />
+      <label for="name" class="form__label">Name</label>
+
+      <label for="bday">Введите дату вашего рождения:</label>
+      <input type="date" id="bday" name="bday" />
+     
+    </form>
+</div>
+  </section>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style lang="scss">
-@use "~/assets/css/style.scss";
+@use "~/assets/css/style.css";
 </style>
