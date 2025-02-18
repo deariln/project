220004f9bce459e74086f7da69bb597cdd84424d
deariln <deariln@mail.ru>
2025-02-18 20:39:36 +0300

    fix+++

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
 
+
   devtools: { enabled: false },
   app: {
     baseURL: '/project/',
+  
     // pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       charset: 'utf-8',
@@ -17,6 +19,10 @@ export default defineNuxtConfig({
       ],
       titleTemplate: '%s - Site Title',
     }
+  },
+
+  nitro: {
+    static: true
   },
 
   vue: {
